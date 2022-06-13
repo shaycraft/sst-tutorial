20d5483d62fdda1201353e2bbaadb763a1d2b7db
Sam Haycraft <phreakdawg@gmail.com>
2022-06-13 00:12:32 -0600

    feat: adds dynamodb table poc

--- backend/functions/lambda.ts
+++ backend/functions/lambda.ts
@@ -1,4 +1,30 @@
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+export async function counterHandler(event: any): Promise<any> {
+  const putParams = {
+    TableName: process.env.tableName || 'default',
+    Key: {
+      ticker: 'hits',
+    },
+    // Update the "tally" column
+    UpdateExpression: 'SET tally = :count',
+    ExpressionAttributeValues: {
+      // Increase the count
+      ':count': 42,
+    },
+  };
+
+  await dynamoDb.update(putParams).promise();
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: `Counter set to 42`,
+  };
+}
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   return {
